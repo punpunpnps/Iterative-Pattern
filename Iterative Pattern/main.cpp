c8ca2af5493d580839fa406237c854fff15b1fcf
punpunpnps <pannaphon.senamontri@torontomu.ca>
2024-02-25 22:22:36 -0500

    Initial Commit

---
+++ Iterative Pattern/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Iterative Pattern
+//
+//  Created by Pannaphon Senamontri on 2/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
