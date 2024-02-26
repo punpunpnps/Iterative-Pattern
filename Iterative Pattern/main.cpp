ddfade4213596d4696e054b5aca1e4407aaf9f9e
punpunpnps <pannaphon.senamontri@torontomu.ca>
2024-02-26 14:28:07 -0500

    Update main.cpp

--- Iterative Pattern/main.cpp
+++ Iterative Pattern/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  Iterative Pattern
-//
-//  Created by Pannaphon Senamontri on 2/25/24.
-//
-
 #include <iostream>
+#include <thread>
+#include <chrono>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void printTile(const std::string& emoji) {
+    std::cout << emoji;
+}
+
+void generatePattern(int rows, int cols) {
+    std::string emojis[][2] = {
+        {"🍀", "🌟"},
+        {"🚀", "🌈"},
+        {"🍄", "💖"},
+        {"🐾", "🎈"},
+    };
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printTile(emojis[i % 4][j % 2]);
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int rows = 8;
+    int cols = 30;
+
+    generatePattern(rows, cols);
+
     return 0;
 }
